c5eb2134d4a751211395f1f026088344a2ed6fe4
Kaue <ksoaresmadarazzo@gmail.com>
2025-03-31 09:08:14 -0300

    declaraçoes de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+var nome = "fiap"
+console.log (nome)
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome = "fiapinho"
+console.log(sobrenome)
+
+// declarando um variavel indefinida
+let nome1;
+console.log(nome1)
+// declarando uma variavel nula
+let nome2=null;
+console.log(nome2)
+
+
